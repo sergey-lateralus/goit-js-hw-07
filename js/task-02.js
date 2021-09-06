3bf01ef2c1f2c85167b727b9e50ca98d8bdfb901
sergey-lateralus <sergey.nezhyn@gmail.com>
2021-09-06 19:49:32 +0300

    Добавляет решение второй задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsEl = document.getElementById('ingredients');
+
+const addIngredients = ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+    liEl.textContent = ingredient;
+    return liEl;
+});
+
+ingredientsEl.append(...addIngredients);
+
